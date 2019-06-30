30d98262fc3a38b9a283fca3070a6021cfcdda91
mazixuan <mazixuan@bytedance.com>
2019-07-01 00:50:15 +0800

    删除无用资源

--- deprecated-chapter1-master/chapter1/src/main/java/com/ss/android/ugc/chapter1/MainActivity.java
+++ deprecated-chapter1-master/chapter1/src/main/java/com/ss/android/ugc/chapter1/MainActivity.java
@@ -7,6 +7,9 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+/**
+ * Fn +F2
+ */
 public class MainActivity extends Activity {
 
     @Override
@@ -21,5 +24,29 @@ public class MainActivity extends Activity {
                 Toast.makeText(MainActivity.this, "点击了第：" + position, Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    @Override protected void onStart() {
+        super.onStart();
+    }
+
+    @Override protected void onResume() {
+        super.onResume();
+    }
+
+    @Override protected void onPause() {
+        super.onPause();
+    }
+
+    @Override protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override protected void onStop() {
+        super.onStop();
+    }
+
+    @Override protected void onRestart() {
+        super.onRestart();
     }
 }
